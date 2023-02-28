f56919f1eb3e69d1b9913bbc3d045f6425fde0fd
FrancoAvila16 <126541186+FrancoAvila16@users.noreply.github.com>
2023-02-28 01:23:01 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,19 @@
+document.getElementById("botonutn").onclick = function(){
+    document.getElementById("utnp").innerHTML="<b>Tecnico Universitario en programación. 2015-2020 </b>.Capacitado para: Analizar un problema de procesamiento de datos y desarrollarlo en un lenguaje apropiado para su resolución por una computadora, seleccionando el algoritmo adecuado, la técnica de procesamiento correspondiente y conformando un programa correctamente estructurado.";
+}
+
+document.getElementById("botonesb3").onclick = function(){
+    document.getElementById("esb3p").innerHTML="<b>Bachiller en ciencias sociales. 2008-2014</b>.Estudiante prearado para temáticas sociales, políticas, económicas, históricas o humanísticas."
+}
+
+document.getElementById("botonexp1").onclick = function(){
+    document.getElementById("exp1").innerHTML="<b>Pasante de Profesorado en informática. 2019-Actualidad</b> Profesor de Informática orientado a brindar sólidos conocimientos y esté en capacidad de desarrollar nuevos programas de enseñanza relacionados con las tecnologías informáticas, permitiendo una aplicación fácil al modelo curricular, que de igual forma, responda a los avances de la disciplina."
+}
+
+document.getElementById("botonexp2").onclick = function(){
+    document.getElementById("exp2").innerHTML="<b>Diseñador grafico. 2013-2019</b>  Creador de diseños usando ilustraciones, tipografía, fotografías y, lo más habitual, programas de artes gráficas. Preparado de diseños preliminares y bocetos para empezar, experimentar con diferentes ideas, colores, estilos y trazados. "
+}
+
+document.getElementById("botonexp3").onclick = function(){
+    document.getElementById("exp3").innerHTML="<b>Empleado. 2010-2013</b> Las funciones principales de este puesto son proporcionar circulación y servicio al cliente a los usuarios de la biblioteca, incluidos, entre otros, el registro de salida de préstamos, el registro, el manejo de efectivo, el registro de entrada de los préstamos y la asistencia con las computadoras y la tecnología."
+}
